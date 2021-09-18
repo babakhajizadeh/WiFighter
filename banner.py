3713f62e2da8a5ecfa3984e38c1305b9dcbaf053
Babak Hajizadeh <babak.dhz@gmail.com>
2021-09-18 23:30:38 +0430

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,42 @@
+white = '\033[97m'
+green = '\033[92m'
+red = '\033[91m'
+yellow = '\033[93m'
+end = '\033[0m'
+info = '\033[93m[!] \033[0m'
+que = '\033[94m[?] \033[0m'
+bad = '\033[91m[-] \033[0m'
+good = '\033[32m[+] \033[0m'
+run = '\033[97m[~] \033[0m'
+
+banner = red + """
+
+     ╔──────────Wifighter Penetration Test Tool────────────╗
+     | Coded by Babak Hajziadeh.                           |
+     | Follow us on instagram.com/babakdhz.                |
+     | This tool is designed for demonstration purpose.    |
+     | Developer has zero responsibility for any potential |
+     | abuse by user.                                      |
+     | This tool is experimental comes with no guarantee   |
+     ┖─────────────────────────────────────────────────────┙
+         VERSION: 
+         FOLLOW ME ON INSTAGRAM:
+         https://www.instagram.com/babakdhz/
+"""
+banner2 = red + """
+
+
+                                  ▓                       ▓
+                                 ▓                         ▓
+                                ▓     ▓               ▓     ▓
+                               ▓     ▓    ▓       ▓    ▓     ▓
+                               ▓    ▓    ▓   ▓▓▓   ▓    ▓    ▓
+                               ▓     ▓    ▓       ▓    ▓     ▓     Wifighter
+                                ▓     ▓               ▓     ▓      version:
+                                 ▓                         ▓       Developed by:
+                                  ▓                       ▓        Babak Hajizadeh
+
+      ╔───────────────────────────────────────────────────────────────────────────╗
+      |                     Wifighter Penetration Test Tool                       |
+      ┖───────────────────────────────────────────────────────────────────────────┙
+"""
